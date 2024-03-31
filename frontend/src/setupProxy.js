7918cd0628876c89a3245a9dc635c27fb3b8acfe
jiheneguesmi <Jihene.Guesmi@etudiant-fst.utm.tn>
2024-03-31 17:42:23 +0200

    Update setupProxy.js to configure proxy for backend API

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: '10.0.4.215:5000', // Change this to match your backend URL
+      target: '10.0.4.100:5000', // Change this to match your backend URL
       changeOrigin: true,
     })
   );
