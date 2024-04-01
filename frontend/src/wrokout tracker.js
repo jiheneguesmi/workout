bb03caf9b9f7558661584c93566c9253827714e3
jiheneguesmi <Jihene.Guesmi@etudiant-fst.utm.tn>
2024-04-02 01:17:54 +0200

    Update setupProxy.js to configure proxy for backend API

--- frontend/src/wrokout tracker.js
+++ frontend/src/wrokout tracker.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://10.0.4.128:5000:5000', // Change this to match your backend URL
+      target: 'http://10.0.172.223:5000', // Change this to match your backend URL
       changeOrigin: true,
     })
   );
